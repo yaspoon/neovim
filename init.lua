71d1b946f4f563d6d05d011afa46c8b004bddbfa
derp <derp@derp>
2026-05-15 15:40:38 +1000

    Add MarkerGroups code annotation plugin
    -Yet another code annotation plugin because I'm not quite happy
     with anything so far

--- init.lua
+++ init.lua
@@ -120,6 +120,27 @@ require("lazy").setup({
         })
     end,
   },
+
+  -- MarkerGroups, yet another code annotation/marker plugin
+  {
+      "jameswolensky/marker-groups.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim", -- Required
+        "ibhagwan/fzf-lua", -- Optional: fzf-lua picker
+        "folke/snacks.nvim", -- Optional: Snacks picker
+        "nvim-telescope/telescope.nvim", -- Optional: Telescope picker
+        -- mini.pick is part of mini.nvim; this plugin vendors mini.nvim for tests,
+        -- but you can also install mini.nvim explicitly to use mini.pick system-wide
+        -- "nvim-mini/mini.nvim",
+      },
+      config = function()
+        require("marker-groups").setup({
+          -- Default picker is 'vim' (built-in vim.ui)
+          -- Accepted values: 'vim' | 'snacks' | 'fzf-lua' | 'mini.pick' | 'telescope'
+          picker = 'telescope',
+        })
+      end,
+  }
 })
 
 -- Telescope ignore files in build
